95662e91460ee57ebc4c44ecd9e3c824d26ff75a
hidr4lisk <federico.furgiuele@gmail.com>
2024-11-27 23:39:55 -0300

    feat: app core y vistas

---
+++ src/core/views.py
@@ -0,0 +1,17 @@
+from django.http import HttpResponse
+from django.shortcuts import render
+
+def saludar(request):
+    return HttpResponse("Hola desde Django!")
+
+def saludar_con_etiqueta(request):
+    return HttpResponse("<h1> Hola ! </h1>")
+
+def saludar_con_parametros(request, nombre: str, apellido: str):
+    nombre = nombre.capitalize()
+    apellido = apellido.capitalize()
+    return HttpResponse(f'{apellido}, {nombre}')
+
+def index(request):
+    context={"año": 2024}
+    return render(request, "core/index.html", context)
