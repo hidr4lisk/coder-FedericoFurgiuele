718702b4f4d9ec839e15dff776f7421df9600ddd
hidr4lisk <federico.furgiuele@gmail.com>
2024-12-03 14:41:50 -0300

    actualización de la base de datos y cosasa

--- src/cliente/models.py
+++ src/cliente/models.py
@@ -1,7 +1,14 @@
 from django.db import models
 
 
-# Create your models here.
+class Pais(models.Model):
+    nombre = models.CharField(max_length=255, unique=True)
+
 class Cliente(models.Model):
     nombre = models.CharField(max_length=255)
     apellido = models.CharField(max_length=255)
+    nacimiento = models.DateField(null=True, blank=True)
+    pais_origen = models.ForeignKey(Pais, null=True, blank=True, on_delete=models.SET_NULL)
+
+    def __str__(self):
+        return f'{self.apellido}, {self.nombre}'
